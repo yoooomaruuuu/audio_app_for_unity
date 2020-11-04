ccbd666703794efcf2c856561aebf647959d1c48
yoooomaruuuu <yoooomaruuuu@gmail.com>
2020-11-05 02:23:08 +0900

    udp受信環境作成に際してコントローラ、udp受信スクリプトの追加 コントローラに関しては後ほど調整

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public bool debug = false;
+
+    GameObject audioSource;
+    AudioManajor audioManajor;
+    Rigidbody2D rigid2D;
+    float jumpForce = 320.0f;
+    float walkSpeed = 0.05f;
+    private Sprite stateSprite;
+    private Sprite runSprite;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //audioSource = GameObject.Find("Audio Source");
+        //audioManajor = audioSource.GetComponent<AudioManajor>();
+        //this.rigid2D = GetComponent<Rigidbody2D>();
+        //stateSprite = Resources.Load<Sprite>("player_state");
+        //runSprite = Resources.Load<Sprite>("player_run");
+        //spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+// #if true
+//         if(Input.GetKeyDown(KeyCode.Space))
+// #else
+//        if(audioManajor.getSamples()[0] > 0.0f)
+// #endif
+//         {
+//             this.rigid2D.AddForce(transform.up * this.jumpForce); 
+//         }
+// 
+// #if false
+//         if (Input.GetKey(KeyCode.RightArrow))
+// #else
+//        if(audioManajor.getFFTOutReal()[0] > 10.0f)
+// #endif
+//         {
+//             this.gameObject.transform.Translate(walkSpeed, 0, 0);
+//             spriteRenderer.sprite = runSprite;
+//         }
+//         else
+//         {
+//             spriteRenderer.sprite = stateSprite;
+//         }
+//         if(debug)
+//         {
+//             if (Input.GetKey(KeyCode.LeftArrow)) this.gameObject.transform.Translate(-1.0f * walkSpeed, 0, 0);;
+//         }
+    }
+}
---
+++ Assets/myNAudioClass.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_audio_analysis;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using System.Threading;
+
+namespace Assets
+{
+    class myNAudioClass : MonoBehaviour
+    {
+        int LOCAL_PORT = 2222;
+        static UdpClient udp;
+        Thread rcv_wave_thread;
+        int frameMsec = 16;
+        ComplexData fftInput;
+        ComplexData fftOutput;
+        List<short> waveBuffer1ch;
+        List<short> waveBuffer2ch;
+        int bufferSize;
+        float[] dataSamples;
+        const int timeLength = 1;
+        const int samplingRate = 48000;
+        FFTFuncs fftClass;
+        float[] powerSpectre;
+
+        void Start()
+        {
+            bufferSize = 1024;//(int)((frameMsec / 1000.0) * samplingRate);
+            waveBuffer1ch = new List<short>();
+            waveBuffer2ch = new List<short>();
+            dataSamples = new float[bufferSize];
+            dataSamples = new float[bufferSize];
+            fftInput.real = new float[bufferSize];
+            fftInput.imaginary = new float[bufferSize];
+            fftOutput.real = new float[bufferSize];
+            fftOutput.imaginary = new float[bufferSize];
+            powerSpectre = new float[bufferSize];
+            fftClass = new FFTFuncs(bufferSize, bufferSize);
+            fftClass.setFFTMode(FFTFuncs.fftMode.FFT);
+
+            udp = new UdpClient(LOCAL_PORT);
+            udp.Client.ReceiveTimeout = 1000;
+            rcv_wave_thread = new Thread(new ThreadStart(waveUdpRcv));
+            rcv_wave_thread.Start();
+        }
+
+        void Update()
+        {
+            if(waveBuffer1ch.Count > bufferSize)
+            {
+                short[] tmp = waveBuffer1ch.GetRange(0, bufferSize).ToArray();
+                dataSamples = Array.ConvertAll(tmp, (x) => x / 32767.0f);
+                fftInput.real = Array.ConvertAll(dataSamples, FFTFuncs.hann_window);
+                fftClass.fftRun(fftInput, fftOutput);
+                waveBuffer1ch.RemoveRange(0, bufferSize);
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
+            rcv_wave_thread.Abort();
+        }
+
+        private void waveUdpRcv()
+        {
+            while(true)
+            {
+                IPEndPoint remoteEP = null;
+                if(udp.Available != 0)
+                {
+                    byte[] data = udp.Receive(ref remoteEP);
+                    for(int i=0; i<data.Length; i+=2)
+                    {
+                        byte[] tmp = new byte[] { data[i], data[i + 1] };
+                        waveBuffer1ch.Add(BitConverter.ToInt16(tmp, 0));
+                    }
+                }
+            }
+        }
+
+        public float[] getSamples()
+        {
+            return dataSamples;
+        }
+        public float[] getFFTOutReal()
+        {
+            //fftInput.real = Array.ConvertAll(getSamples(), FFTFuncs.hann_window);
+            //FFTFuncs.fftException exc = fftClass.fftRun(fftInput, fftOutput);
+            return fftOutput.real;
+        }
+
+        //public float[] getPowerSpectre()
+        //{
+        //    calcPowerSpectre();
+        //    return  powerSpectre;
+        //}
+
+        //public int getRecordPosition()
+        //{
+        //    return Microphone.GetPosition(null);
+        //}
+
+        public int getFFTSize()
+        {
+            return fftClass.getFFTSize();
+        }
+
+        public int getTimeLength()
+        {
+            return timeLength;
+        }
+
+        public int getSamplingRate()
+        {
+            return samplingRate;
+        }
+        //return dbv
+        //private void calcPowerSpectre()
+        //{
+        //    for(int i=0; i<powerSpectre.Length; i++)
+        //    {
+        //        powerSpectre[i] = (float)(Math.Pow(fftOutput.real[i], 2.0) + Math.Pow(fftOutput.imaginary[i], 2.0)) / (float)powerSpectre.Length;
+        //    }
+        //}
+    }
+}
